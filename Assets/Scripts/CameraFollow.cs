67cb60439dae1c76b85b3b931b7634172b3efd25
Wajahat-Rana <wajahatsaleem59@gmail.com>
2024-08-20 13:17:23 +0500

    Made Camera Follow Player Within Bounds

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject player;
+    private PlayerScore playerScore;
+    private float minX = 0f, maxX = 70f;
+
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+        playerScore = player.GetComponent<PlayerScore>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerScore.isAlive){
+            Vector3 temp = transform.position;
+            temp.x = player.transform.position.x;
+            if(temp.x < minX){
+                temp.x = minX;
+            }
+
+            if(temp.x > maxX){
+                temp.x = maxX;
+            }
+            temp.y = player.transform.position.y;
+            transform.position = temp;
+        }
+    }
+}
